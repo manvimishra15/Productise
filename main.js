5b25104dcd3111d6ccc0fbec819de6452cfc5775
manvimishra15 <btbtc24098_manvi@banasthali.in>
2025-12-28 12:09:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  console.log("🚀 App is launching...");  // 👈 add this line to see if it runs
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
